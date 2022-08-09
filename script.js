f7ddc4f08a2c612dbbcc842bdb983635fc8098b8
isak <isakfreires98@gmail.com>
2022-08-09 17:22:57 -0300

    implement 5 round functionality

--- script.js
+++ script.js
@@ -17,8 +17,8 @@
                 break;            
         }
     }
-    const computerChoice = getComputerChoice();
-    console.log(computerChoice);
+    // const computerChoice = getComputerChoice();
+    // console.log(computerChoice);
 
 //Create a function that plays a single round of Rock Paper Scissors.
 //it should take two parameters - the playerSelection and computerSelection and return
@@ -38,4 +38,17 @@
             return "You won!";
         }
     }
-    console.log(playSingleRound(computerChoice));
+    // console.log(playSingleRound(computerChoice));
+
+// Write a NEW function called game(). Call the playRound function inside 
+// of this one to play a 5 round game that keeps score and reports a winner 
+// or loser at the end.
+
+    function game(){
+        for(let i = 0; i < 5; i++){
+            const computerChoice = getComputerChoice();
+            console.log(playSingleRound(computerChoice));
+        }
+    }
+
+    game();
